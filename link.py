eb93e050fb4d76c75966181a0056a0b26c5b6bd3
Cory Li <51144+Cixelyn@users.noreply.github.com>
2015-02-08 18:25:11 -0800

    getting dotfiles to play nicely on windows

--- link.py
+++ link.py
@@ -1,7 +1,21 @@
 import argparse
 
-from os import listdir, symlink, unlink
+from os import listdir, unlink
 from os.path import isfile, dirname, join, expanduser, abspath
+
+def symlink(source, link_name):
+    import os
+    os_symlink = getattr(os, "symlink", None)
+    if callable(os_symlink):
+        os_symlink(source, link_name)
+    else:
+        import ctypes
+        csl = ctypes.windll.kernel32.CreateSymbolicLinkW
+        csl.argtypes = (ctypes.c_wchar_p, ctypes.c_wchar_p, ctypes.c_uint32)
+        csl.restype = ctypes.c_ubyte
+        flags = 1 if os.path.isdir(source) else 0
+        if csl(link_name, source, flags) == 0:
+            raise ctypes.WinError()
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
